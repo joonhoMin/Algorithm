6a3bf11a362884de26529dfbad19977ce86c5671
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2025-02-26 12:03:11 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
+    return 0;
+}
