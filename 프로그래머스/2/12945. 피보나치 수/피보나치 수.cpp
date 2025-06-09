6d59e08af014e1fe45031a57908e822f0b4f1f0b
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2025-06-09 23:31:43 +0900

    [level 2] Title: 피보나치 수, Time: 0.60 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    vector<int> v(100001);
+    
+    v[0]=0;
+    v[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        v[i]=(v[i-1]+v[i-2])%1234567;
+    }
+    
+    answer=v[n];
+    
+    return answer;
+}
