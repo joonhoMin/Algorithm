58d8d14bb1ff9a2ebf28274b25b5c8238016839c
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2025-08-03 15:31:06 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    while(n>0)
+    {
+        if(n%2==0)
+            n/=2;
+        else
+        {
+            ans+=1;
+            n-=1;
+        }
+    }
+
+    return ans;
+}
