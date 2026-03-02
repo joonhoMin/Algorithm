ebb23b61adacb19d6f8bc11be00e1d59800c7d2d
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2026-03-02 19:27:21 +0900

    [level 2] Title: 괄호 회전하기, Time: 2.90 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -3,49 +3,40 @@
 using namespace std;
 
 int solution(string s) {
+    int n = s.length();
     int answer = 0;
-    
-    stack<char> stk;
-    
-    for(int i=0;i<s.length();i++)
-    {
-        int flag=1;
-        
-        while(!stk.empty())
-            stk.pop();
-        for(int j=0;j<s.length();j++)
-        {
-           if(s[j]=='('||s[j]=='{'||s[j]=='[')
-               stk.push(s[j]);
-           else if(s[j]==')'||s[j]=='}'||s[j]==']')
-           {
-               if(stk.empty())
-               {
-                   flag=0;
-                   break;
-               }
-               else if(s[j]==')'&&stk.top()=='(')
-                   stk.pop();
-               else if(s[j]=='}'&&stk.top()=='{')
-                   stk.pop();
-               else if(s[j]==']'&&stk.top()=='[')
-                   stk.pop();
-               else
-               {
-                   flag=0;
-                   break;
-               }
-           }
+
+    string t = s + s;
+
+    for (int i = 0; i < n; i++) {
+        stack<char> stk;
+        bool valid = true;
+
+        for (int j = i; j < i + n; j++) {
+            char c = t[j];
+
+            if (c == '(' || c == '{' || c == '[')
+                stk.push(c);
+            else {
+                if (stk.empty()) {
+                    valid = false;
+                    break;
+                }
+
+                if ((c == ')' && stk.top() == '(') ||
+                    (c == '}' && stk.top() == '{') ||
+                    (c == ']' && stk.top() == '['))
+                    stk.pop();
+                else {
+                    valid = false;
+                    break;
+                }
+            }
         }
-        if(!stk.empty())
-            flag=0;
-        if(flag==1)
-            answer+=1;
-        char tmp=s[0];
-        for(int j=0;j<s.length()-1;j++)
-            s[j]=s[j+1];
-        s[s.length()-1]=tmp;
+
+        if (valid && stk.empty())
+            answer++;
     }
-    
+
     return answer;
 }
