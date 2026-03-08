54f6147f2ad811613f78801920500e86186d8a4c
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2026-03-08 16:15:36 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.86 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -4,25 +4,20 @@ using namespace std;
 
 int binaryOneCount(int n)
 {
-    string binary;
     int count=0;
     
     while(n>0)
     {
-        binary+=to_string(n%2);
+        if(n%2==1)
+            count+=1;
         n/=2;
     }
-    
-    for(int i=0;i<binary.length();i++)
-        if(binary[i]=='1')
-            count+=1;
     
     return count;
     
 }
 
 int solution(int n) {
-    int answer = 0;
     
     int count = binaryOneCount(n);
     
@@ -30,8 +25,8 @@ int solution(int n) {
     {
         n=n+1;
         if(count==binaryOneCount(n))
-            return n;
+            break;
     }
     
-    return answer;
+    return n;
 }
