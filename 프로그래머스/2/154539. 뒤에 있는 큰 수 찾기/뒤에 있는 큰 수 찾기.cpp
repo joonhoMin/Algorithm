17be240f1eb02841a78ca53ff935501b7c477f40
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2026-03-09 23:59:58 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 380.28 ms, Memory: 240 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    int n = numbers.size();
+    vector<int> answer(n, -1);
+    stack<int> st; // 인덱스 저장
+
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && numbers[st.top()] < numbers[i]) {
+            answer[st.top()] = numbers[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    return answer;
+}
