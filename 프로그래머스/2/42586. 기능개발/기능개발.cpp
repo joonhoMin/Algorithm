668552cc6fe605a936c8346b521372ecb16c2c17
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2026-06-11 23:26:22 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -4,27 +4,32 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> days;
-
-    for(int i=0;i<progresses.size();i++){
-        int d = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-        days.push_back(d);
+    
+    vector<int> remain;
+    
+    for(int i=0;i<progresses.size();i++)
+    {
+        remain.push_back((100-progresses[i]+speeds[i]-1)/speeds[i]);
     }
-
-    int current = days[0];
-    int count = 1;
-
-    for(int i=1;i<days.size();i++){
-        if(days[i] <= current){
-            count++;
-        } else {
-            answer.push_back(count);
-            current = days[i];
-            count = 1;
+    //7 3 9
+    
+    int cnt=1;
+    int piv=0;
+    for(int i=1;i<remain.size();i++)
+    {
+        if(remain[i]>remain[piv])
+        {
+            answer.push_back(cnt);
+            cnt=1;
+            piv=i;
+        }
+        else
+        {
+            cnt+=1;
         }
     }
-
-    answer.push_back(count);
-
+    
+    answer.push_back(cnt);
+    
     return answer;
 }
