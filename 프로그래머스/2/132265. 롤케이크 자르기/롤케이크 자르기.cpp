49255bf85bacfd0114bb2b6a99a2da15bd650196
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2026-06-14 20:28:39 +0900

    [level 2] Title: 롤케이크 자르기, Time: 5.05 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,56 +1,36 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int solution(vector<int> topping) {
-
-    int answer = 0;  // 공평하게 자르는 경우의 수
-
-    // 오른쪽 케이크의 토핑 개수
-    vector<int> right(10001, 0);
-
-    // 왼쪽 케이크의 토핑 개수
-    vector<int> left(10001, 0);
-
-    int leftKind = 0;   // 왼쪽 케이크의 토핑 종류 수
-    int rightKind = 0;  // 오른쪽 케이크의 토핑 종류 수
-
-
-    // -----------------------------
-    // 처음에는 모든 토핑이 오른쪽 케이크에 있음
-    // -----------------------------
-    for(int t : topping)
+    int answer = 0;
+    
+    vector<int> right(10001,0);
+    vector<int> left(10001,0);
+    
+    int leftkind=0, rightkind=0;
+    
+    for(int i=0;i<topping.size();i++)
     {
-        if(right[t] == 0)   // 처음 등장한 토핑이면
-            rightKind++;    // 종류 증가
-
-        right[t]++;         // 해당 토핑 개수 증가
+        if(right[topping[i]]==0)
+            rightkind++;
+        right[topping[i]]++;
     }
-
-
-    // -----------------------------
-    // 토핑을 하나씩 왼쪽으로 이동
-    // -----------------------------
-    for(int t : topping)
+    
+    for(int i=0;i<topping.size();i++)
     {
-        // 왼쪽 케이크에 추가
-        if(left[t] == 0)    // 처음 등장한 토핑이면
-            leftKind++;     // 종류 증가
-
-        left[t]++;          // 토핑 개수 증가
-
-
-        // 오른쪽 케이크에서 제거
-        right[t]--;
-
-        if(right[t] == 0)   // 더 이상 없으면
-            rightKind--;    // 종류 감소
-
-
-        // 두 케이크의 토핑 종류 수 비교
-        if(leftKind == rightKind)
+        if(left[topping[i]]==0)
+            leftkind++;
+        left[topping[i]]++;
+        
+        right[topping[i]]--;
+        
+        if(right[topping[i]]==0)
+            rightkind--;
+        
+        if(leftkind==rightkind)
             answer++;
     }
-
     
     return answer;
 }
