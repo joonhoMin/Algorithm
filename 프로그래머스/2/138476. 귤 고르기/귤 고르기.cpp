d7f31af3d7fc0b9916111ecdd7ac52bf556b4514
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2026-06-25 22:04:13 +0900

    [level 2] Title: 귤 고르기, Time: 24.22 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,47 +1,28 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
-    
-    unordered_map<int,int> m;
-    
-    for(int i:tangerine)
-        m[i]++;
-    /*
-    m[1]=1
-    m[2]=2
-    m[3]=2
-    m[4]=1
-    m[5]=2
-    크기가 1인 귤 1개, ... 크키가 5인 귤 2개
-    */
-    
-    int n=tangerine.size();
-    
-    vector<int> v(n+1);
-    
-    for(auto i:m)
-        v[i.second]++;
-    /*
-    크기는 상관없이 갯수가 많은거 순서대로 넣어야 하니
-    v[2]=3
-    v[1]=2
-    2개의 같은 크기를 가진 귤이 3종류, 1개의 같은 크기를 가진 귤이     2종류
-    */
-    
-    //가장 갯수가 많은 크기의 귤부터 빼야 하니 역순으로
-    for(int i=n;i>=1;i--)
-    {
-        //한 종류씩 담으면서 k에서 빼줌
-        while(v[i]>0)
-        {
-            k-=i;
-            v[i]-=1;
-            answer+=1;
-            if(k<=0)
-            return answer;
+
+    unordered_map<int, int> m;
+    for (int x : tangerine) {
+        m[x]++;
+    }
+
+    map<int, int, greater<int>> m2;
+    for (auto [size, cnt] : m) {
+        m2[cnt]++;
+    }
+
+    for (auto [cnt, kind] : m2) {
+        while (kind > 0) {
+            k -= cnt;
+            answer++;
+            kind--;
+
+            if (k <= 0) return answer;
         }
     }
+
+    return answer;
 }
